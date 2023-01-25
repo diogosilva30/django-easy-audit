c3c8bf8d6cfcecfdf572271e044b29ab1ec6048d
Diogo Silva <diogosilv30@gmail.com>
2023-01-25 21:41:14 +0000

    Fix Performance issues in CRUDEvent

--- easyaudit/admin.py
+++ easyaudit/admin.py
@@ -2,35 +2,66 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 
-try: # Django 2.0
+try:  # Django 2.0
     from django.urls import reverse
-except: # Django < 2.0
+except:  # Django < 2.0
     from django.core.urlresolvers import reverse
 
-from django.utils.safestring import mark_safe
 from django.utils.html import escape
+from django.utils.safestring import mark_safe
+
 from . import settings
+from .admin_helpers import EasyAuditModelAdmin, prettify_json
 from .models import CRUDEvent, LoginEvent, RequestEvent
-from .admin_helpers import prettify_json, EasyAuditModelAdmin
-from .settings import (CRUD_EVENT_LIST_FILTER, LOGIN_EVENT_LIST_FILTER, REQUEST_EVENT_LIST_FILTER,
-                       CRUD_EVENT_SEARCH_FIELDS, LOGIN_EVENT_SEARCH_FIELDS, REQUEST_EVENT_SEARCH_FIELDS,
-                       READONLY_EVENTS)
+from .settings import (
+    CRUD_EVENT_LIST_FILTER,
+    CRUD_EVENT_SEARCH_FIELDS,
+    LOGIN_EVENT_LIST_FILTER,
+    LOGIN_EVENT_SEARCH_FIELDS,
+    READONLY_EVENTS,
+    REQUEST_EVENT_LIST_FILTER,
+    REQUEST_EVENT_SEARCH_FIELDS,
+)
+
 
 # CRUD events
 class CRUDEventAdmin(EasyAuditModelAdmin):
-    list_display = ['get_event_type_display', 'get_content_type', 'object_id', 'object_repr_link', 'user_link', 'datetime']
-    date_hierarchy = 'datetime'
+    show_full_result_count = False
+    list_display = [
+        # "get_event_type_display",
+        # "get_content_type",
+        "object_id",
+        "object_repr_link",
+        "user_link",
+        "datetime",
+    ]
+    date_hierarchy = "datetime"
+    date_hierarchy_drilldown = False
+
     list_filter = CRUD_EVENT_LIST_FILTER
     search_fields = CRUD_EVENT_SEARCH_FIELDS
-    readonly_fields = ['event_type', 'object_id', 'get_content_type',
-                       'object_repr', 'object_json_repr_prettified', 'get_user',
-                       'user_pk_as_string', 'datetime', 'changed_fields_prettified']
-    exclude = ['object_json_repr', 'changed_fields']
+    readonly_fields = [
+        "event_type",
+        "object_id",
+        "get_content_type",
+        "object_repr",
+        "object_json_repr_prettified",
+        "get_user",
+        "user_pk_as_string",
+        "datetime",
+        "changed_fields_prettified",
+    ]
+    exclude = ["object_json_repr", "changed_fields"]
 
     def get_changelist_instance(self, *args, **kwargs):
         changelist_instance = super().get_changelist_instance(*args, **kwargs)
-        content_type_ids = [obj.content_type_id for obj in changelist_instance.result_list]
-        self.content_types_by_id = {content_type.id: content_type for content_type in ContentType.objects.filter(id__in=content_type_ids)}
+        content_type_ids = [
+            obj.content_type_id for obj in changelist_instance.result_list
+        ]
+        self.content_types_by_id = {
+            content_type.id: content_type
+            for content_type in ContentType.objects.filter(id__in=content_type_ids)
+        }
         return changelist_instance
 
     def get_content_type(self, obj):
@@ -50,26 +81,30 @@ class CRUDEventAdmin(EasyAuditModelAdmin):
             escaped_obj_repr = escape(obj.object_repr)
             try:
                 content_type = self.get_content_type(obj)
-                url = reverse("admin:%s_%s_change" % (
-                    content_type.app_label,
-                    content_type.model,
-                ), args=(obj.object_id,))
+                url = reverse(
+                    "admin:%s_%s_change"
+                    % (
+                        content_type.app_label,
+                        content_type.model,
+                    ),
+                    args=(obj.object_id,),
+                )
                 html = '<a href="%s">%s</a>' % (url, escaped_obj_repr)
             except Exception:
                 html = escaped_obj_repr
         return mark_safe(html)
 
-    object_repr_link.short_description = 'object repr'
+    object_repr_link.short_description = "object repr"
 
     def object_json_repr_prettified(self, obj):
         return prettify_json(obj.object_json_repr)
 
-    object_json_repr_prettified.short_description = 'object json repr'
+    object_json_repr_prettified.short_description = "object json repr"
 
     def changed_fields_prettified(self, obj):
         return prettify_json(obj.changed_fields)
 
-    changed_fields_prettified.short_description = 'changed fields'
+    changed_fields_prettified.short_description = "changed fields"
 
 
 if settings.ADMIN_SHOW_MODEL_EVENTS:
@@ -78,11 +113,23 @@ if settings.ADMIN_SHOW_MODEL_EVENTS:
 
 # Login events
 class LoginEventAdmin(EasyAuditModelAdmin):
-    list_display = ['datetime', 'get_login_type_display', 'user_link', "get_username", 'remote_ip']
-    date_hierarchy = 'datetime'
+    list_display = [
+        "datetime",
+        "get_login_type_display",
+        "user_link",
+        "get_username",
+        "remote_ip",
+    ]
+    date_hierarchy = "datetime"
     list_filter = LOGIN_EVENT_LIST_FILTER
     search_fields = LOGIN_EVENT_SEARCH_FIELDS
-    readonly_fields = ['login_type', 'get_username', 'get_user', 'remote_ip', 'datetime', ]
+    readonly_fields = [
+        "login_type",
+        "get_username",
+        "get_user",
+        "remote_ip",
+        "datetime",
+    ]
 
     def get_user(self, obj):
         return self.users_by_id.get(obj.user_id)
@@ -103,11 +150,18 @@ if settings.ADMIN_SHOW_AUTH_EVENTS:
 
 # Request events
 class RequestEventAdmin(EasyAuditModelAdmin):
-    list_display = ['datetime', 'user_link', 'method', 'url', 'remote_ip']
-    date_hierarchy = 'datetime'
+    list_display = ["datetime", "user_link", "method", "url", "remote_ip"]
+    date_hierarchy = "datetime"
     list_filter = REQUEST_EVENT_LIST_FILTER
     search_fields = REQUEST_EVENT_SEARCH_FIELDS
-    readonly_fields = ['url', 'method', 'query_string', 'get_user', 'remote_ip', 'datetime', ]
+    readonly_fields = [
+        "url",
+        "method",
+        "query_string",
+        "get_user",
+        "remote_ip",
+        "datetime",
+    ]
 
     def get_user(self, obj):
         return self.users_by_id.get(obj.user_id)
--- easyaudit/models.py
+++ easyaudit/models.py
@@ -18,31 +18,58 @@ class CRUDEvent(models.Model):
     M2M_CLEAR_REV = 11
 
     TYPES = (
-        (CREATE, _('Create')),
-        (UPDATE, _('Update')),
-        (DELETE, _('Delete')),
-        (M2M_CHANGE, _('Many-to-Many Change')),
-        (M2M_CHANGE_REV, _('Reverse Many-to-Many Change')),
-        (M2M_ADD, _('Many-to-Many Add')),
-        (M2M_ADD_REV, _('Reverse Many-to-Many Add')),
-        (M2M_REMOVE, _('Many-to-Many Remove')),
-        (M2M_REMOVE_REV, _('Reverse Many-to-Many Remove')),
-        (M2M_CLEAR, _('Many-to-Many Clear')),
-        (M2M_CLEAR_REV, _('Reverse Many-to-Many Clear')),
+        (CREATE, _("Create")),
+        (UPDATE, _("Update")),
+        (DELETE, _("Delete")),
+        (M2M_CHANGE, _("Many-to-Many Change")),
+        (M2M_CHANGE_REV, _("Reverse Many-to-Many Change")),
+        (M2M_ADD, _("Many-to-Many Add")),
+        (M2M_ADD_REV, _("Reverse Many-to-Many Add")),
+        (M2M_REMOVE, _("Many-to-Many Remove")),
+        (M2M_REMOVE_REV, _("Reverse Many-to-Many Remove")),
+        (M2M_CLEAR, _("Many-to-Many Clear")),
+        (M2M_CLEAR_REV, _("Reverse Many-to-Many Clear")),
     )
 
-    event_type = models.SmallIntegerField(choices=TYPES, verbose_name=_('Event type'))
-    object_id = models.CharField(max_length=255, verbose_name=_('Object ID'))
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, db_constraint=False, verbose_name=_('Content type'))
-    object_repr = models.TextField(null=True, blank=True, verbose_name=_('Object representation'))
-    object_json_repr = models.TextField(null=True, blank=True, verbose_name=_('Object JSON representation'))
-    changed_fields = models.TextField(null=True, blank=True, verbose_name=_('Changed fields'))
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
-                             blank=True, on_delete=models.SET_NULL,
-                             db_constraint=False, verbose_name=_('User'))
-    user_pk_as_string = models.CharField(max_length=255, null=True, blank=True,
-                                         help_text=_('String version of the user pk'), verbose_name=_('User PK as string'))
-    datetime = models.DateTimeField(auto_now_add=True, verbose_name=_('Date time'))
+    event_type = models.SmallIntegerField(choices=TYPES, verbose_name=_("Event type"))
+    object_id = models.CharField(
+        max_length=255, verbose_name=_("Object ID"), db_index=True
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+        verbose_name=_("Content type"),
+    )
+    object_repr = models.TextField(
+        null=True, blank=True, verbose_name=_("Object representation")
+    )
+    object_json_repr = models.TextField(
+        null=True, blank=True, verbose_name=_("Object JSON representation")
+    )
+    changed_fields = models.TextField(
+        null=True, blank=True, verbose_name=_("Changed fields")
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        db_constraint=False,
+        verbose_name=_("User"),
+    )
+    user_pk_as_string = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        help_text=_("String version of the user pk"),
+        verbose_name=_("User PK as string"),
+    )
+    datetime = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Date time"),
+        db_index=True,
+    )
 
     def is_create(self):
         return self.CREATE == self.event_type
@@ -54,10 +81,15 @@ class CRUDEvent(models.Model):
         return self.DELETE == self.event_type
 
     class Meta:
-        verbose_name = _('CRUD event')
-        verbose_name_plural = _('CRUD events')
-        ordering = ['-datetime']
-        index_together = ['object_id', 'content_type', ]
+        verbose_name = _("CRUD event")
+        verbose_name_plural = _("CRUD events")
+        ordering = ["-datetime"]
+        indexes = [
+            # models.Index(fields=["object_id", "content_type"]),
+            models.Index(fields=["object_id", "object_repr"]),
+            models.Index(fields=["object_id"]),
+            models.Index(fields=["object_repr"]),
+        ]
 
 
 class LoginEvent(models.Model):
@@ -65,35 +97,63 @@ class LoginEvent(models.Model):
     LOGOUT = 1
     FAILED = 2
     TYPES = (
-        (LOGIN, _('Login')),
-        (LOGOUT, _('Logout')),
-        (FAILED, _('Failed login')),
+        (LOGIN, _("Login")),
+        (LOGOUT, _("Logout")),
+        (FAILED, _("Failed login")),
     )
-    login_type = models.SmallIntegerField(choices=TYPES, verbose_name=_('Event type'))
-    username = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('Username'))
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True,
-                             on_delete=models.SET_NULL, db_constraint=False,
-                             verbose_name=_('User'))
-    remote_ip = models.CharField(max_length=50, null=True, db_index=True, verbose_name=_('Remote IP'))
-    datetime = models.DateTimeField(auto_now_add=True, verbose_name=_('Date time'))
+    login_type = models.SmallIntegerField(choices=TYPES, verbose_name=_("Event type"))
+    username = models.CharField(
+        max_length=255, null=True, blank=True, verbose_name=_("Username")
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        db_constraint=False,
+        verbose_name=_("User"),
+    )
+    remote_ip = models.CharField(
+        max_length=50, null=True, db_index=True, verbose_name=_("Remote IP")
+    )
+    datetime = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Date time"),
+        db_index=True,
+    )
 
     class Meta:
-        verbose_name = _('login event')
-        verbose_name_plural = _('login events')
-        ordering = ['-datetime']
+        verbose_name = _("login event")
+        verbose_name_plural = _("login events")
+        ordering = ["-datetime"]
 
 
 class RequestEvent(models.Model):
-    url = models.CharField(null=False, db_index=True, max_length=254, verbose_name=_('URL'))
-    method = models.CharField(max_length=20, null=False, db_index=True, verbose_name=_('Method'))
-    query_string = models.TextField(null=True, verbose_name=_('Query string'))
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True,
-                             on_delete=models.SET_NULL, db_constraint=False,
-                             verbose_name=_('User'))
-    remote_ip = models.CharField(max_length=50, null=True, db_index=True, verbose_name=_('Remote IP'))
-    datetime = models.DateTimeField(auto_now_add=True, verbose_name=_('Date time'))
+    url = models.CharField(
+        null=False, db_index=True, max_length=254, verbose_name=_("URL")
+    )
+    method = models.CharField(
+        max_length=20, null=False, db_index=True, verbose_name=_("Method")
+    )
+    query_string = models.TextField(null=True, verbose_name=_("Query string"))
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        db_constraint=False,
+        verbose_name=_("User"),
+    )
+    remote_ip = models.CharField(
+        max_length=50, null=True, db_index=True, verbose_name=_("Remote IP")
+    )
+    datetime = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Date time"),
+        db_index=True,
+    )
 
     class Meta:
-        verbose_name = _('request event')
-        verbose_name_plural = _('request events')
-        ordering = ['-datetime']
+        verbose_name = _("request event")
+        verbose_name_plural = _("request events")
+        ordering = ["-datetime"]
